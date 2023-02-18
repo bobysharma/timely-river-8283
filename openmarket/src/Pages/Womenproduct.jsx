ab655bf2ac6e54faeb374e4bceb8036fbfb5babc
Boby Sharma <69707812+bobysharma@users.noreply.github.com>
2023-02-19 03:00:22 +0530

    woment page update

--- openmarket/src/Pages/Womenproduct.jsx
+++ openmarket/src/Pages/Womenproduct.jsx
@@ -1,11 +1,71 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useReducer } from "react";
+import ProductCard from "../Component/ProductCard";
+const initialstate={
+    womenproduct:[],
+    isloading:true,
+    isErr:"",
+}
+const reducer=(state,action)=>{
+switch(action.type){
+case "success":
+    return{
+        ...state,
+        womenproduct:action.payload,
+        isloading:false,
+        isErr:'',
+    }
+    case "failed":
+        return{
+            womenproduct:[],
+            isloading:false,
+            isErr:"something went wrong",
+        }
+        default:
+            throw new Error();
+
+}
+}
 const Womenproduct=()=>{
+    const[state,dispatch]=useReducer(reducer,initialstate)
+    const{womenproduct,isError,isLoading}=state; 
+    const getData=async()=>{
+        axios.get('http://localhost:8080/female').then((res)=>{
+            dispatch({type:"success",payload:res.data})
+        }).catch((err)=>{
+            dispatch({type:"failed",payload:err})
+
+        }) 
+
+    }
+    useEffect(()=>{
+        getData();
+
+    },[])
     return(
         <>
-        <hr width="80%"  height="50%" style={{
-  border: "3px solid #5DAEF2",
-  borderRadius: "5px",
-  marginTop:"45px"
- }}/>
+       <Grid templateColumns="repeat(4,1fr)" gap={6} width="100%">
+        {
+            womenproduct.map((item)=>(
+              <GridItem  key={item.id}  w="100%" h="100%" bg="blue.500">
+                <ProductCard id={item.id} brand={item.brand} 
+                description={item.description} 
+                name={item.name}
+                price={item.price}
+                 images={item.images}
+                 strikeOfPrice={item.strikeOfPrice}
+                > 
+                
+                </ProductCard>
+               
+              </GridItem>
+            )
+
+             )
+        }
+        
+        </Grid>
         </>
     )
 }
